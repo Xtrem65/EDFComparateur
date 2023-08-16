61c1656a84ffc0ce9df3431cdde8649efe6ca574
Nicolas BORDERES <nicolas.borderes@gmail.com>
2023-08-16 22:04:34 +0200

    Ajout d'une fonction pour aller chercher et memoriser le dataset de l'ODRE sur la conso et l'impact carbone de chaque tranche de 15min sur une journee

--- app/api_rte.py
+++ app/api_rte.py
@@ -2,8 +2,130 @@ import urllib
 import requests
 
 from api_config import *
-# Merci https://github.com/ReinboldV/api_rte
 
+########  ODRE #########
+"""
+
+Details des champs, un peu reorganisés à partir d'un exemple
+"fields": {
+"consommation": 36414,
+"taux_co2": 55,
+
+"nucleaire": 29374,
+
+"eolien_terrestre": "ND",
+"eolien": 1707,
+"eolien_offshore": "ND",
+
+"hydraulique": 4652,
+"hydraulique_lacs": 579,
+"hydraulique_fil_eau_eclusee": 3820
+"hydraulique_step_turbinage": 253,
+"pompage": -1631,
+
+"gaz": 3599,
+"gaz_tac": 0,
+"gaz_autres": 0,
+"gaz_ccg": 3179,
+"gaz_cogen": 422,
+
+"fioul": 143,
+"fioul_cogen": 29,
+"fioul_tac": 1,
+"fioul_autres": 113,
+
+"charbon": 6,
+
+"ech_physiques": -2435,
+
+"bioenergies": 999,
+"bioenergies_dechets": 168,
+"bioenergies_biomasse": 560,
+"bioenergies_biogaz": 276,
+
+"solaire": 0,
+"stockage_batterie": "ND",
+"destockage_batterie": "ND",
+
+"heure": "04:15",
+"nature": "Données temps réel",
+"date_heure": "2022-06-01T02:15:00+00:00",
+"date": "2022-06-01",
+"prevision_j1": 36150,
+"prevision_j": 36400,
+"perimetre": "France",
+},
+"""
+GLOBAL_PRODUCTION_DATA_DETAILS = {}
+
+def getProductionDetails(date):
+    if date in GLOBAL_PRODUCTION_DATA_DETAILS:
+        print ("Found Data for"+ date)
+    else: 
+        GLOBAL_PRODUCTION_DATA_DETAILS[date] = {}
+        details = requests.get('https://opendata.reseaux-energies.fr/api/records/1.0/search/'
+            '?dataset=eco2mix-national-tr&rows=96&sort=-date'
+            '&facet=date_heure&refine.date_heure=%s' % date).json()
+        dataset= details["records"]
+        for horodateDataSet in dataset:
+            cleanedData = {}
+            #On s'en tape des autres données
+            horodateDataSet = horodateDataSet["fields"]
+            print("Looking at new dataset")
+            print(horodateDataSet["date"])
+            print(horodateDataSet["heure"])
+            cleanedData["CO2"] = horodateDataSet["taux_co2"]
+            conso = horodateDataSet["consommation"]
+            cleanedData["conso"] = conso
+
+            nukePercent = (horodateDataSet["nucleaire"] / conso) * 100
+            cleanedData["nukePercent"] = nukePercent
+            
+            # On récupère le volume eolien
+            #On ignore horodateDataSet["eolien_terrestre"] + horodateDataSet["eolien_offshore"]
+            totalEolien = horodateDataSet["eolien"] 
+            eolienPercent = (totalEolien / conso) * 100
+            cleanedData["eolienPercent"] = eolienPercent
+
+            # On récupère le volume PV
+            # On considère le stockage/destockage batterie comme de l'eolien
+            totalSolaire = horodateDataSet["solaire"] #+ horodateDataSet["stockage_batterie"] + horodateDataSet["destockage_batterie"]
+            solairePercent = (totalSolaire / conso) * 100
+            cleanedData["solairePercent"] = solairePercent
+
+
+            # On récupère le volume hydro
+            totalHydro = horodateDataSet["hydraulique"] + horodateDataSet["pompage"]
+            hydrauPercent = (totalHydro / conso) * 100
+            cleanedData["hydrauPercent"] = hydrauPercent
+
+            # On récupère le volume biomasse
+            totalBioEnergies = horodateDataSet["bioenergies"]
+            bioenergiesPercent = (totalBioEnergies / conso) * 100
+            cleanedData["bioenergiesPercent"] = bioenergiesPercent
+
+            # On récupère le volume fossile
+            totalFossile = horodateDataSet["charbon"] + horodateDataSet["fioul"] + horodateDataSet["gaz"]
+            fossilePercent = (totalFossile / conso) * 100
+            cleanedData["fossilePercent"] = fossilePercent
+
+            # On récupère le volume d'échanges
+            totalEchanges = horodateDataSet["ech_physiques"]
+            echangesPercent = (totalEchanges / conso) * 100
+            cleanedData["echangesPercent"] = echangesPercent
+
+            checkSommePourcent = nukePercent+eolienPercent+hydrauPercent+solairePercent+bioenergiesPercent+fossilePercent+echangesPercent
+            print("Total : %f " % checkSommePourcent)
+            print("Now remembering CleanedData")
+            print(cleanedData)
+            
+            GLOBAL_PRODUCTION_DATA_DETAILS[date][horodateDataSet["heure"]] = cleanedData
+
+    return GLOBAL_PRODUCTION_DATA_DETAILS[date]
+
+######## RTE / TEMPO #########
+
+# Merci https://github.com/ReinboldV/api_rte
 def get_token(oauth_url=OAUTH_URL, client_id=EDF_CLIENT_ID, client_secret=EDF_CLIENT_SECRET):
     """
 
