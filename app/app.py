eb171cfc120dbb41d689fccb07f717245c0d691d
Nicolas BORDERES <nicolas.borderes@gmail.com>
2023-08-24 22:49:33 +0200

    Ajour argparse avec param debug pour définir si on reste en mode local/debug/http ou si on passe en mode prod https

--- app/app.py
+++ app/app.py
@@ -2,6 +2,12 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 from AnalyseConso import *
 from io import StringIO
+import argparse
+parser = argparse.ArgumentParser(description="Just an example",
+	formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("-d", "--debug", action="store_true", help="debug mode")
+parser.add_argument("-v", "--verbose", action="store_true", help="increase verbosity")
+args = parser.parse_args()
 
 @app.route('/', methods=["GET", "POST"])
 def homepage():
@@ -21,4 +27,7 @@ def homepage():
 	return render_template("homepage.html") #Ici, on peut upload son fichier, cliquer sur un bouton pour processer et être redirigé vers la page de resultats
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=8000, ssl_context='adhoc', debug=True)
+	if args.debug == True :
+		app.run(host='0.0.0.0', port=8000, debug=True)
+	else:
+		app.run(host='0.0.0.0', port=8000, ssl_context='adhoc')
