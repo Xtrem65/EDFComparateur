bd8408ef86bba36f7378be8da963f6669da85d06
Nicolas BORDERES <nicolas.borderes@gmail.com>
2023-08-26 13:24:46 +0200

    On passe le poids CO2 en kg

--- app/earthWatcher.py
+++ app/earthWatcher.py
@@ -202,7 +202,7 @@ class EarthWatcher:
     def getTotalConso(self):
         return round(self.totalConso,2)
     def getCO2(self):
-        return round(self.totalCO2,2)
+        return round(self.totalCO2,2)/1000
     
     def getValueByOrigin(self):
         return {
